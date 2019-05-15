247b29fb9919d1e4ba88883f42e6c8c00577901b
micaeprado <micaelaprado94@gmail.com>
2019-05-15 12:47:46 -0300

    ajustes

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -1,10 +1,6 @@
-const argv = require('./config/yargs').argv;
 const axios = require('axios');
 
-console.log(argv.direccion);
-
 const encodeURL = encodeURI(argv.direccion);
-console.log(encodeURL);
 
 const instance = axios.create({
     baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeURL}`,
